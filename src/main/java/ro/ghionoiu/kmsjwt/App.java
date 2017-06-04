537d027d4554e2857711d0e5317c88195e01d12e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-06-04 05:30:04 +0100

    Create the JWT Encoder and Decoder

--- src/main/java/ro/ghionoiu/kmsjwt/App.java
+++ src/main/java/ro/ghionoiu/kmsjwt/App.java
@@ -1,8 +1,20 @@
 package ro.ghionoiu.kmsjwt;
 
+import io.jsonwebtoken.Claims;
+import ro.ghionoiu.kmsjwt.key.DummyKeyObfuscation;
+import ro.ghionoiu.kmsjwt.token.JWTDecoder;
+import ro.ghionoiu.kmsjwt.token.JWTEncoder;
+import ro.ghionoiu.kmsjwt.token.JWTVerificationException;
+
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws JWTVerificationException {
+        String jwt = JWTEncoder.builder(new DummyKeyObfuscation())
+                .claim("usr", "friendly_name")
+                .compact();
+
+        Claims claims = new JWTDecoder(new DummyKeyObfuscation())
+                .decodeAndVerify(jwt);
+        System.out.println("usr: "+claims.get("usr"));
     }
 }
